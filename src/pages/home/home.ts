bcdf55b8920fca97f62dda45b3d455c39cdc8a0b
Juan Tavares <juantavares2006@hotmail.com>
2018-12-04 22:49:33 -0300

    Navegando para a outra página

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 import { Carro } from '../../models/carro';
 import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -49,5 +50,6 @@ export class HomePage {
 
   selecionaCarro(carro:Carro) {
     console.log(carro);
+    this.navCtrl.push(EscolhaPage)
   }
 }
