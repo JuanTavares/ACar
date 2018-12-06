1edc508502a003afc17683db962ee014444ce6a0
Juan Tavares <juantavares2006@hotmail.com>
2018-12-06 20:12:12 -0300

    Criando método para atualizar preço total
    À partir dos preços dos acessórios selecionados somados com o preço do carro.

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../models/carro';
+import { Acessorio } from '../../models/acessorio';
 
 @IonicPage()
 @Component({
@@ -10,15 +11,23 @@ import { Carro } from '../../models/carro';
 export class EscolhaPage {
 
   carro: Carro;
-  acessorios = [];
+  acessorios: Acessorio[];
+  precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carroSelecionado');
+    this.precoTotal = this.carro.preco;
     this.acessorios = [
       { nome: 'Freio ABS', preco: 800 },
       { nome: 'Ar-condicionado', preco: 1000 },
       { nome: 'MP3 Player', preco: 500 }
     ];
+  }
+
+  atualizaTotal(toggleState: boolean, acessorio: Acessorio) {
+    toggleState ?
+      this.precoTotal += acessorio.preco
+      : this.precoTotal -= acessorio.preco;
   }
 
 }
