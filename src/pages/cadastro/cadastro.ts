df0c3be6783414da247b85b095d86f741a333eda
Juan Tavares <juantavares2006@hotmail.com>
2018-12-10 19:35:51 -0300

    Fazendo navegação para home page depois de um agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Alert, AlertController } from 'ionic-angular';
 import { Carro } from '../../models/carro';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -42,7 +43,12 @@ export class CadastroPage {
 
     this.alerta = this.alertCtrl.create({
       buttons: [
-        { text: 'ok' }
+        {
+          text: 'ok',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
       ]
     });
 
