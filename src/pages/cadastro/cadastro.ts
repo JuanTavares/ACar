b66e4642796d0ba3331fd1f63969d6728450188c
Juan Tavares <juantavares2006@hotmail.com>
2018-12-10 19:23:12 -0300

    Enviando agendamento ao back-end e exibindo mensagem ao usuário

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Alert, AlertController } from 'ionic-angular';
 import { Carro } from '../../models/carro';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
 @IonicPage()
 @Component({
@@ -17,20 +18,47 @@ export class CadastroPage {
   email: string = '';
   data: string = new Date().toISOString();
 
-  constructor(public navCtrl: NavController,
-    public navParams: NavParams
+  alerta: Alert;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private agendamentosService: AgendamentosServiceProvider,
+    private alertCtrl: AlertController
   ) {
     this.carro = this.navParams.get('carroSelecionado');
     this.precoTotal = this.navParams.get('precoTotal');
   }
 
   agendar() {
-    console.log({
-      nome: this.nome,
-      endereco: this.endereco,
-      email: this.email,
+    let agendamento = {
+      nomeCliente: this.nome,
+      enderecoCliente: this.endereco,
+      emailCliente: this.email,
+      modeloCarro: this.carro,
+      precoTotal: this.precoTotal,
       data: this.data
+    };
+
+    this.alerta = this.alertCtrl.create({
+      buttons: [
+        { text: 'ok' }
+      ]
     });
+
+    this.agendamentosService.agenda(agendamento)
+      .subscribe(
+        () => {
+          this.alerta.setTitle('Parabéns!');
+          this.alerta.setSubTitle('Agendamento realizado com sucesso.');
+          this.alerta.present();
+        },
+        () => {
+          this.alerta.setTitle('Aviso');
+          this.alerta.setSubTitle('Falha no agendamento! Tente novamente mais tarde.');
+          this.alerta.present();
+        }
+      );
   }
 
 }
