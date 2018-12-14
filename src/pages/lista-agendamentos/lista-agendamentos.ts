354ac13a2220c8549bbb2842eb07ecc879a45c85
Juan Tavares <juantavares2006@hotmail.com>
2018-12-14 20:56:20 -0300

    Configurando método para reenviar agendamento falho

--- src/pages/lista-agendamentos/lista-agendamentos.ts
+++ src/pages/lista-agendamentos/lista-agendamentos.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { AgendamentoDaoProvider } from '../../providers/agendamento-dao/agendamento-dao';
 import { Agendamento } from '../../models/agendamento';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
 @IonicPage()
 @Component({
@@ -11,10 +12,13 @@ import { Agendamento } from '../../models/agendamento';
 export class ListaAgendamentosPage {
 
   agendamentos: Agendamento[];
+  alerta: Alert;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private agendamentosService: AgendamentosServiceProvider,
     private agendamentoDao: AgendamentoDaoProvider
   ) { }
 
@@ -25,6 +29,48 @@ export class ListaAgendamentosPage {
           this.agendamentos = agendamentos;
         }
       )
+  }
+
+  reenvia(agendamento: Agendamento) {
+
+    let mensagem = {
+      title: '',
+      subtitle: ''
+    };
+
+    this.alerta = this.alertCtrl.create({
+      buttons: [
+        {
+          text: 'ok'
+        }
+      ]
+    });
+
+    this.agendamentosService.agenda(agendamento)
+      .mergeMap((valor) => {
+        let observable = this.agendamentoDao.salva(agendamento);
+        if (valor instanceof Error) {
+          throw valor;
+        }
+        return observable;
+      })
+      .finally(
+        () => {
+          this.alerta.setTitle(mensagem.title);
+          this.alerta.setSubTitle(mensagem.subtitle);
+          this.alerta.present();
+        }
+      )
+      .subscribe(
+        () => {
+          mensagem.title = 'Parabéns!';
+          mensagem.subtitle = 'Agendamento reenviado com sucesso.';
+        },
+        (err: Error) => {
+          mensagem.title = 'Aviso';
+          mensagem.subtitle = err.message;
+        }
+      );
   }
 
 }
